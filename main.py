11d3ff35ac1898840c310fc2a4ff684bc7b48715
Vanidasan <vanidasanr@gmail.com>
2025-11-07 17:05:59 +0530

    🔁 Updated main.py — logic refined

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-print("Hello, World!")  
-print("This is a simple Python script.")
-print("It prints multiple lines to the console.")   
+import xlwings as xw
+import pandas as pd
+import os
+import random
+
+def Phase1_testing():
+    print("Phase 1 Testing")
+
+    print("Hi Vani")
+
+def main():
+    Phase1_testing()
+
+if __name__ == "__main__":
+    main()
